5035eff20e6bedd0b816b0579ecf5492a40fd00a
JeffersonCabello <jefcsanc@espol.edu.ec>
2024-07-22 22:47:37 -0500

    A mimir

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -43,6 +43,15 @@ class _ImageBuble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.70,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text("Ella Freya esta enviando una imagen..."),
+          );
+        },
       ),
     );
   }
