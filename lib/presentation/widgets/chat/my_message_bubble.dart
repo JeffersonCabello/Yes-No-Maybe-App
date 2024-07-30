d9510e5093e906e26553c1625e88cb0d51731c76
Jefferson Cabello <jefcsanc@espol.edu.ec>
2024-07-30 10:22:50 -0500

    Que facil es

--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
- 
+import 'package:yes_no_app/domain/entities/message.dart';
+
 class MyMessageBubble extends StatelessWidget {
-  const MyMessageBubble({Key? key}) : super(key: key);
+  final Message message;
+  const MyMessageBubble({Key? key, required this.message}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.end,
-      children: [Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20.0),
-        color: colors.primary),
-      child: const Padding(padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-      child: Text('La revolucion industrial y sus consecuencias han sido un desastre para la raza humana',style: TextStyle(color: Colors.white),)),
-    ),
-    const SizedBox(height: 10,)],);
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: colors.primary),
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                message.text,
+                style: const TextStyle(color: Colors.white),
+              )),
+        ),
+        const SizedBox(
+          height: 10,
+        )
+      ],
+    );
   }
 }
